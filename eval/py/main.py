fc8ff4da88afb4b8789979ed6b06b20686e6b0c0
Youssef Altherwy <y.altherwy17@alumni.imperial.ac.uk>
2025-05-06 17:53:07 +0300

    start ontonotes

--- eval/py/main.py
+++ eval/py/main.py
@@ -2,8 +2,8 @@
 import convert_to_tide as ct
 import extract_annotations_to_jsonl as ea
 #%%
-original_dataset = '../data/original_conll_dataset.csv'
-output_dataset = '../data/conll_ground_truth.jsonl'
+original_dataset = '../data/ontonotes_dataset.csv'
+output_dataset = '../data/ontonotes_ground_truth.jsonl'
 ct.convert_to_tide_format(original_dataset, output_dataset)
 # %%
 # export each line in the jsonl file to a separate .txt file
