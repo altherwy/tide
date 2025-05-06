2c7cf36c3474122221fc217ba4aaf1df613c5d33
Youssef Altherwy <y.altherwy17@alumni.imperial.ac.uk>
2025-05-06 19:10:05 +0300

    generate prediction files

--- eval/py/main.py
+++ eval/py/main.py
@@ -8,15 +8,15 @@ ct.convert_to_tide_format(original_dataset, output_dataset)
 # %%
 # export each line in the jsonl file to a separate .txt file
 import generate_input_txt as git
-output_dir = '../data/sample_notes'
+output_dir = '../../sample_notes'
 git.split_jsonl_to_txt(output_dataset, output_dir)
 # %% Generate prediction file
-annotation_files_path = '../../output/conll/annotator'
-output_path = '../data/conll_predictions.jsonl'
+annotation_files_path = '../../output/ontonotes/annotator'
+output_path = '../data/ontonotes_predictions.jsonl'
 ea.extract_annotations_to_jsonl(annotation_files_path, output_path)
 # %% calculate metrics MASKED
 import calculate_metrics as ct
-gt_file_path = '../data/conll_ground_truth.jsonl'
+gt_file_path = '../data/ontonotes_ground_truth.jsonl'
 pred_file_path = '../data/conll_predictions.jsonl'
 tp = ct.calc_tp(gt_file_path, pred_file_path)
 gt_total_labels = ct.get_total_labels(gt_file_path)
